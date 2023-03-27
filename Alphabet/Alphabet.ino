6ff2c2f4c47b4a935d28fcb5a5903e13f631c922
LukeMech <lukaszjajkommz@gmail.com>
2023-03-27 23:11:26 +0200

    Add some letters and dynamic colors support

--- Alphabet/Alphabet.ino
+++ Alphabet/Alphabet.ino
@@ -2,10 +2,8 @@
 
 // <-- Settings - required -->
 
-#define LED_PIN D1  // LED Pin
-
-#define LED_COLOR_0 strip.Color(0, 0, 0)   // diode color for 0 (background) (R G B)
-#define LED_COLOR_1 strip.Color(20, 0, 0)  // diode color for 1 (text) (R G B)
+#define LED_PIN D1                                           // LED Pin
+Adafruit_NeoPixel strip(64, LED_PIN, NEO_GRB + NEO_KHZ800);  // Init LEDs
 
 uint8_t led_map[8][8] = {  // Table corresponding to the physical position/number of the LEDs
   { 63, 62, 61, 60, 59, 58, 57, 56 },
@@ -18,6 +16,8 @@ uint8_t led_map[8][8] = {  // Table corresponding to the physical position/numbe
   { 0, 1, 2, 3, 4, 5, 6, 7 }
 };
 
+uint32_t LED_COLOR_0 = strip.Color(0, 0, 0);   // diode color for 0 (background) (R G B)
+uint32_t LED_COLOR_1 = strip.Color(20, 0, 0);  // diode color for 1 (text) (R G B)
 
 // <-- To use out-of-the-box -->
 
@@ -26,10 +26,12 @@ struct {
   uint8_t
 
     A[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+
     B[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
     C[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
     D[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
     E[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+
     F[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
     G[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 1, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
     H[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
@@ -40,10 +42,20 @@ struct {
     Ł[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 1, 0, 0, 0 }, { 0, 0, 1, 1, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
     M[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0, 0, 0, 1, 0 }, { 0, 1, 1, 0, 0, 1, 1, 0 }, { 0, 1, 0, 1, 1, 0, 1, 0 }, { 0, 1, 0, 0, 0, 0, 1, 0 }, { 0, 1, 0, 0, 0, 0, 1, 0 }, { 0, 1, 0, 0, 0, 0, 1, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
     N[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0, 0, 0, 1, 0 }, { 0, 1, 1, 0, 0, 0, 1, 0 }, { 0, 1, 0, 1, 0, 0, 1, 0 }, { 0, 1, 0, 0, 1, 0, 1, 0 }, { 0, 1, 0, 0, 0, 1, 1, 0 }, { 0, 1, 0, 0, 0, 0, 1, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
-    O[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
-    P[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
 
-    U[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } };
+    O[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+
+    P[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+    R[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 1, 0, 0, 0, 0 }, { 0, 0, 1, 0, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+    S[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1, 1, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 0, 0, 0, 0 }, { 0, 0, 0, 0, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+    T[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 1, 1, 1, 1, 1, 0 }, { 0, 1, 1, 1, 1, 1, 1, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+    U[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+    W[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0, 0, 0, 1, 0 }, { 0, 1, 0, 0, 0, 0, 1, 0 }, { 0, 1, 0, 0, 0, 0, 1, 0 }, { 0, 1, 0, 1, 1, 0, 1, 0 }, { 0, 1, 1, 0, 0, 1, 1, 0 }, { 0, 1, 0, 0, 0, 0, 1, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+    X[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+    Y[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0, 0, 0, 1, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+    Z[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 1, 0, 0, 0 }, { 0, 0, 0, 1, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+    Ź[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 1, 0, 0, 0 }, { 0, 0, 0, 1, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 1, 0, 0, 0 }, { 0, 0, 0, 1, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+    Ż[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 1, 0, 0, 0 }, { 0, 0, 0, 1, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } };
 
 } alphabet;
 
@@ -62,9 +74,6 @@ struct {
 
 } numbers;
 
-// Init LEDs
-Adafruit_NeoPixel strip(64, LED_PIN, NEO_GRB + NEO_KHZ800);
-
 // Display static map
 void display(uint8_t map[][8]) {
   for (uint8_t i = 0; i < 8; i++) {
@@ -79,18 +88,32 @@ void display(uint8_t map[][8]) {
 }
 
 // Animate between maps
-void animate(uint8_t startMap[][8], uint8_t endMap[][8], uint8_t direction = 0, int gap = 50, int addDelay = 0) {
+void animate(uint8_t startMap[][8], uint8_t endMap[][8], uint8_t direction = 0, int gap = 50, uint32_t newColor1 = 0, uint32_t newColor0 = 0) {
+
+  uint32_t oldColor0 = LED_COLOR_0, oldColor1 = LED_COLOR_1, color0, color1;
+  // New background color
+  if (newColor0) LED_COLOR_0 = newColor0;
+  // New text color
+  if (newColor1) LED_COLOR_1 = newColor1;
+
   // Direction 0: Text ---->>>
   if (!direction) {
     for (uint8_t shift = 1; shift < 8; shift++) {
       for (uint8_t y = 0; y < 8; y++) {
         for (uint8_t x = 0; x < 8; x++) {
           uint8_t value = 0;
-          if (x < (7 - shift)) value = startMap[y][x + shift];
-          else value = endMap[y][x - (7 - shift)];
+          if (x < (7 - shift)) {
+            value = startMap[y][x + shift];
+            color0 = oldColor0;
+            color1 = oldColor1;
+          } else {
+            value = endMap[y][x - (7 - shift)];
+            color0 = LED_COLOR_0;
+            color1 = LED_COLOR_1;
+          }
           uint8_t index = led_map[y][x];
-          if (value == 0) strip.setPixelColor(index, LED_COLOR_0);
-          else if (value == 1) strip.setPixelColor(index, LED_COLOR_1);
+          if (value == 0) strip.setPixelColor(index, color0);
+          else if (value == 1) strip.setPixelColor(index, color1);
         }
       }
       strip.show();
@@ -98,61 +121,174 @@ void animate(uint8_t startMap[][8], uint8_t endMap[][8], uint8_t direction = 0, 
     }
   }
   // Direction 1: <<<---- Text
-  else {
+  else if (direction == 1) {
     for (int8_t shift = 6; shift >= 0; shift--) {
       for (uint8_t y = 0; y < 8; y++) {
         for (uint8_t x = 0; x < 8; x++) {
           uint8_t value = 0;
-          if (x >= (7 - shift)) value = startMap[y][x - (7 - shift)];
-          else value = endMap[y][x + shift];
+          if (x >= (7 - shift)) {
+            value = startMap[y][x - (7 - shift)];
+            color0 = oldColor0;
+            color1 = oldColor1;
+          } else {
+            value = endMap[y][x + shift];
+            color0 = LED_COLOR_0;
+            color1 = LED_COLOR_1;
+          }
           uint8_t index = led_map[y][x];
-          if (value == 0) strip.setPixelColor(index, LED_COLOR_0);
-          else if (value == 1) strip.setPixelColor(index, LED_COLOR_1);
+          if (value == 0) strip.setPixelColor(index, color0);
+          else if (value == 1) strip.setPixelColor(index, color1);
         }
       }
       strip.show();
       delay(gap);
     }
   }
-
-  delay(addDelay);
+  // Direction 2:  ^ Text ^
+  else if (direction == 2) {
+    for (uint8_t shift = 1; shift < 8; shift++) {
+      for (uint8_t x = 0; x < 8; x++) {
+        for (uint8_t y = 0; y < 8; y++) {
+          uint8_t value = 0;
+          if (y < (7 - shift)) {
+            value = startMap[y + shift][x];
+            color0 = oldColor0;
+            color1 = oldColor1;
+          } else {
+            value = endMap[y - (7 - shift)][x];
+            color0 = LED_COLOR_0;
+            color1 = LED_COLOR_1;
+          }
+          uint8_t index = led_map[y][x];
+          if (value == 0) strip.setPixelColor(index, color0);
+          else if (value == 1) strip.setPixelColor(index, color1);
+        }
+      }
+      strip.show();
+      delay(gap);
+    }
+  }
+  // Direction 3:  \/ Text \/
+  else if (direction == 3) {
+    for (int8_t shift = 6; shift >= 0; shift--) {
+      for (uint8_t x = 0; x < 8; x++) {
+        for (uint8_t y = 0; y < 8; y++) {
+          uint8_t value = 0;
+          if (y >= (7 - shift)) {
+            value = startMap[y - (7 - shift)][x];
+            color0 = oldColor0;
+            color1 = oldColor1;
+          } else {
+            value = endMap[y + shift][x];
+            color0 = LED_COLOR_0;
+            color1 = LED_COLOR_1;
+          }
+          uint8_t index = led_map[y][x];
+          if (value == 0) strip.setPixelColor(index, color0);
+          else if (value == 1) strip.setPixelColor(index, color1);
+        }
+      }
+      strip.show();
+      delay(gap);
+    }
+  }
 }
 
 void noiceAlphabet(int gaps = 0, int animLength = 90) {
 
-  animate(alphabet.A, alphabet.B, 0, animLength, gaps);
-  animate(alphabet.B, alphabet.C, 0, animLength, gaps);
-  animate(alphabet.C, alphabet.D, 0, animLength, gaps);
-  animate(alphabet.D, alphabet.E, 0, animLength, gaps);
-  animate(alphabet.E, alphabet.F, 0, animLength, gaps);
-  animate(alphabet.F, alphabet.G, 0, animLength, gaps);
-  animate(alphabet.G, alphabet.H, 0, animLength, gaps);
-  animate(alphabet.H, alphabet.I, 0, animLength, gaps);
-  animate(alphabet.I, alphabet.J, 0, animLength, gaps);
-  animate(alphabet.J, alphabet.K, 0, animLength, gaps);
-  animate(alphabet.K, alphabet.L, 0, animLength, gaps);
-  animate(alphabet.L, alphabet.Ł, 0, animLength, gaps);
-  animate(alphabet.Ł, alphabet.M, 0, animLength, gaps);
-  animate(alphabet.M, alphabet.N, 0, animLength, gaps);
-  animate(alphabet.N, alphabet.O, 0, animLength, gaps);
-  animate(alphabet.O, alphabet.P, 0, animLength, gaps);
+  animate(alphabet.A, alphabet.B, 0, animLength);
+  delay(gaps);
+  animate(alphabet.B, alphabet.C, 0, animLength);
+  delay(gaps);
+  animate(alphabet.C, alphabet.D, 0, animLength);
+  delay(gaps);
+  animate(alphabet.D, alphabet.E, 0, animLength);
+  delay(gaps);
+  animate(alphabet.E, alphabet.F, 0, animLength);
+  delay(gaps);
+  animate(alphabet.F, alphabet.G, 0, animLength);
+  delay(gaps);
+  animate(alphabet.G, alphabet.H, 0, animLength);
+  delay(gaps);
+  animate(alphabet.H, alphabet.I, 0, animLength);
+  delay(gaps);
+  animate(alphabet.I, alphabet.J, 0, animLength);
+  delay(gaps);
+  animate(alphabet.J, alphabet.K, 0, animLength);
+  delay(gaps);
+  animate(alphabet.K, alphabet.L, 0, animLength);
+  delay(gaps);
+  animate(alphabet.L, alphabet.M, 0, animLength);
+  delay(gaps);
+  animate(alphabet.M, alphabet.N, 0, animLength);
+  delay(gaps);
+  animate(alphabet.N, alphabet.O, 0, animLength);
+  delay(gaps);
+  animate(alphabet.O, alphabet.P, 0, animLength);
+  delay(gaps);
+  animate(alphabet.P, alphabet.R, 0, animLength);
+  delay(gaps);
+  animate(alphabet.R, alphabet.S, 0, animLength);
+  delay(gaps);
+  animate(alphabet.S, alphabet.T, 0, animLength);
+  delay(gaps);
+  animate(alphabet.T, alphabet.U, 0, animLength);
+  delay(gaps);
+  animate(alphabet.U, alphabet.W, 0, animLength);
+  delay(gaps);
+  animate(alphabet.W, alphabet.X, 0, animLength);
+  delay(gaps);
+  animate(alphabet.X, alphabet.Y, 0, animLength);
+  delay(gaps);
+  animate(alphabet.Y, alphabet.Z, 0, animLength);
+  delay(gaps + 200);
 
-  animate(alphabet.P, alphabet.O, 1, animLength, gaps);
-  animate(alphabet.O, alphabet.N, 1, animLength, gaps);
-  animate(alphabet.N, alphabet.M, 1, animLength, gaps);
-  animate(alphabet.M, alphabet.Ł, 1, animLength, gaps);
-  animate(alphabet.Ł, alphabet.L, 1, animLength, gaps);
-  animate(alphabet.L, alphabet.K, 1, animLength, gaps);
-  animate(alphabet.K, alphabet.J, 1, animLength, gaps);
-  animate(alphabet.J, alphabet.I, 1, animLength, gaps);
-  animate(alphabet.I, alphabet.H, 1, animLength, gaps);
-  animate(alphabet.H, alphabet.G, 1, animLength, gaps);
-  animate(alphabet.G, alphabet.F, 1, animLength, gaps);
-  animate(alphabet.F, alphabet.E, 1, animLength, gaps);
-  animate(alphabet.E, alphabet.D, 1, animLength, gaps);   
-  animate(alphabet.D, alphabet.C, 1, animLength, gaps);
-  animate(alphabet.C, alphabet.B, 1, animLength, gaps);
-  animate(alphabet.B, alphabet.A, 1, animLength, gaps);
+  animate(alphabet.Z, alphabet.Y, 1, animLength);
+  delay(gaps);
+  animate(alphabet.Y, alphabet.X, 1, animLength);
+  delay(gaps);
+  animate(alphabet.X, alphabet.W, 1, animLength);
+  delay(gaps);
+  animate(alphabet.W, alphabet.U, 1, animLength);
+  delay(gaps);
+  animate(alphabet.U, alphabet.T, 1, animLength);
+  delay(gaps);
+  animate(alphabet.T, alphabet.S, 1, animLength);
+  delay(gaps);
+  animate(alphabet.S, alphabet.R, 1, animLength);
+  delay(gaps);
+  animate(alphabet.R, alphabet.P, 1, animLength);
+  delay(gaps);
+  animate(alphabet.P, alphabet.O, 1, animLength);
+  delay(gaps);
+  animate(alphabet.O, alphabet.N, 1, animLength);
+  delay(gaps);
+  animate(alphabet.N, alphabet.M, 1, animLength);
+  delay(gaps);
+  animate(alphabet.M, alphabet.L, 1, animLength);
+  delay(gaps);
+  animate(alphabet.L, alphabet.K, 1, animLength);
+  delay(gaps);
+  animate(alphabet.K, alphabet.J, 1, animLength);
+  delay(gaps);
+  animate(alphabet.J, alphabet.I, 1, animLength);
+  delay(gaps);
+  animate(alphabet.I, alphabet.H, 1, animLength);
+  delay(gaps);
+  animate(alphabet.H, alphabet.G, 1, animLength);
+  delay(gaps);
+  animate(alphabet.G, alphabet.F, 1, animLength);
+  delay(gaps);
+  animate(alphabet.F, alphabet.E, 1, animLength);
+  delay(gaps);
+  animate(alphabet.E, alphabet.D, 1, animLength);
+  delay(gaps);
+  animate(alphabet.D, alphabet.C, 1, animLength);
+  delay(gaps);
+  animate(alphabet.C, alphabet.B, 1, animLength);
+  delay(gaps);
+  animate(alphabet.B, alphabet.A, 1, animLength);
+  delay(gaps + 200);
 }
 
 void setup() {
@@ -160,11 +296,14 @@ void setup() {
   strip.show();
   display(characters.exclam_mark);
   delay(1000);
+  display(alphabet.C);
+  delay(600);
 }
-
 
 void loop() {
   // Here paste the functions you like to use
-
-  noiceAlphabet();
+  animate(alphabet.C, alphabet.B, 2, 120, strip.Color(0, 0, 20));
+  animate(alphabet.B, alphabet.A, 2, 120, strip.Color(0, 20, 0));
+  animate(alphabet.A, alphabet.B, 3, 120, strip.Color(0, 0, 20));
+  animate(alphabet.B, alphabet.C, 3, 120, strip.Color(20, 0, 0));
 }
--- README.md
+++ README.md
@@ -7,8 +7,9 @@ Available functions:
 - **animate()**:
     - **required** startMap (int8_t): map for animation start
     - **required** endMap (int8_t): map for animation end
-    - Direction (0/1) *default is 0 - left-to-right*,
-    - Gaps between frames (in miliseconds) *default is 100*
+    - Direction (0/1/2/3) *default is 0 - left-to-right, 2 is to up*,
+    - New text color (strip.Color),
+    - New background color (strip.Color)
 
 - **display()**:
     - **required** map (int8_t): map to display
