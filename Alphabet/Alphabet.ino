5f063653885c1ff0c532bc16502a9ae119205b48
LukeMech <lukaszjajkommz@gmail.com>
2023-03-27 16:04:28 +0200

    Add letters, bugfix for animation

--- Alphabet/Alphabet.ino
+++ Alphabet/Alphabet.ino
@@ -5,7 +5,7 @@
 #define LED_PIN D1                          // LED Pin
 
 #define LED_COLOR_0 strip.Color(0, 0, 0)    // diode color for 0 (background) (R G B)
-#define LED_COLOR_1 strip.Color(0, 15, 10)  // diode color for 1 (text) (R G B)
+#define LED_COLOR_1 strip.Color(20, 20, 0)  // diode color for 1 (text) (R G B)
 
 uint8_t led_map[8][8] = {                   // Table corresponding to the physical position/number of the LEDs
   { 63, 62, 61, 60, 59, 58, 57, 56 },
@@ -23,11 +23,22 @@ uint8_t led_map[8][8] = {                   // Table corresponding to the physic
 
 // Alphabet maps
 struct Maps {
-  uint8_t A[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } };
-  uint8_t B[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } };
-  uint8_t C[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } };
-  uint8_t D[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } };
-  uint8_t E[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } };
+  uint8_t 
+  A[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+  B[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+  C[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+  D[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 0, 0, 1, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+  E[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+  F[8][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } },
+  G[8][8] = { 
+    { 0, 0, 0, 0, 0, 0, 0, 0 }, 
+    { 0, 0, 0, 1, 1, 0, 0, 0 }, 
+    { 0, 0, 1, 0, 0, 1, 0, 0 }, 
+    { 0, 0, 1, 0, 0, 0, 0, 0 }, 
+    { 0, 0, 1, 0, 1, 1, 0, 0 }, 
+    { 0, 0, 1, 0, 0, 1, 0, 0 }, 
+    { 0, 0, 0, 1, 1, 0, 0, 0 }, 
+    { 0, 0, 0, 0, 0, 0, 0, 0 } };
 
 } alphabet;
 
@@ -49,7 +60,7 @@ void display(uint8_t map[][8]) {
 void animate(uint8_t startMap[][8], uint8_t endMap[][8], uint8_t direction=0, uint8_t gap=100) {
   // Direction 0: Text ---->>>
   if(!direction) {
-    for (uint8_t shift = 0; shift < 7; shift++) {
+    for (uint8_t shift = 1; shift < 7; shift++) {
       for (uint8_t y = 0; y < 8; y++) {
         for (uint8_t x = 0; x < 8; x++) {
           uint8_t value = 0;
@@ -66,7 +77,7 @@ void animate(uint8_t startMap[][8], uint8_t endMap[][8], uint8_t direction=0, ui
   }
   // Direction 1: <<<---- Text
   else {
-    for (int8_t shift = 6; shift >= 0; shift--) {
+    for (int8_t shift = 5; shift >= 0; shift--) {
       for (uint8_t y = 0; y < 8; y++) {
         for (uint8_t x = 0; x < 8; x++) {
           uint8_t value = 0;
@@ -92,22 +103,31 @@ void setup() {
 // Here paste the functions you like to use
 
 void loop() {
+  int del=500;
 
   animate(alphabet.A, alphabet.B);
-  delay(500);
+  delay(del);
   animate(alphabet.B, alphabet.C);
-  delay(500);
+  delay(del);
   animate(alphabet.C, alphabet.D);
-  delay(500);
+  delay(del);
   animate(alphabet.D, alphabet.E);
-  delay(500);
-  animate(alphabet.E, alphabet.D, 1);
-  delay(500);
-  animate(alphabet.D, alphabet.C, 1);
-  delay(500);
-  animate(alphabet.C, alphabet.B, 1);
-  delay(500);
-  animate(alphabet.B, alphabet.A, 1);
-  delay(500);
+  delay(del);
+  animate(alphabet.E, alphabet.F);
+  delay(del);
+  animate(alphabet.F, alphabet.G);
+  delay(del);
 
+  animate(alphabet.G, alphabet.F, 1);
+  delay(del);
+  animate(alphabet.F, alphabet.E, 1);
+  delay(del);
+  animate(alphabet.E, alphabet.D, 1);
+  delay(del);
+  animate(alphabet.D, alphabet.C, 1);
+  delay(del);
+  animate(alphabet.C, alphabet.B, 1);
+  delay(del);
+  animate(alphabet.B, alphabet.A, 1);
+  delay(del);
 }
