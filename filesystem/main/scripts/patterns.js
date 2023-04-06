890ee29aa22d05965680e31aa9729fd724ae8ac3
LukeMech <lukaszjajkommz@gmail.com>
2023-04-06 10:19:23 +0200

    Add options box

--- filesystem/main/scripts/patterns.js
+++ filesystem/main/scripts/patterns.js
@@ -1,12 +1,16 @@
 
 const submitBtn = document.getElementById("submit")
 const textInput = document.getElementById("text")
+const optionsBtn = document.getElementById('optionsBtn')
+const optionsBox = document.getElementById("optionsBox")
+
+let  characters
+let optionsPerAnim = []
 
 submitBtn.addEventListener("click", async function () {
     if (!textInput.value) return;
 
-    const characters = await document.getElementById("text").value.toUpperCase().split("");
-
+    characters = await document.getElementById("text").value.toUpperCase().split("");
     submitBtn.style.borderColor = "#0ad826"    
     textInput.value = ""
 
@@ -22,7 +26,7 @@ submitBtn.addEventListener("click", async function () {
             delay: 0,
         });
 
-        let urlSearchParams = new URLSearchParams(params);
+        const urlSearchParams = new URLSearchParams(params);
         if(i===0) urlSearchParams.append("start", true);
         else if(i===characters.length) urlSearchParams.append("end", true);        
         await request("LEDs/changePattern", urlSearchParams.toString())
@@ -31,6 +35,22 @@ submitBtn.addEventListener("click", async function () {
     setTimeout(() => {
         submitBtn.style.borderColor = ""
     }, 1000);
+});
+
+optionsBtn.addEventListener("click", async function () {
+    if (!textInput.value) return;
+
+    characters = await document.getElementById("text").value.toUpperCase().split("");
+    if (!optionsBox.style.height) {
+        optionsBox.style.height = `${optionsBox.scrollHeight}px`
+        optionsBtn.style.rotate = '180deg'
+    }
+    else {
+        optionsBtn.style.rotate = ''
+        optionsBox.style.height = '';
+    }
+
+    // More code soon
 });
 
 
